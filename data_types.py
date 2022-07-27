95d146d90ed87f45c93e4ad4c386afc966cc1191
dlcastra <1dlcastra@gmail.com>
2022-07-27 12:09:44 +0300

    completed first task

--- data_types.py
+++ data_types.py
@@ -1,1 +1,29 @@
-print("check this")
+# first example
+
+a = 2
+b = 6
+c = a + b
+print("c =", c, type(c))
+
+# second example
+
+fstg = "some words"
+skstg = "more words"
+manywd = (skstg + " " + fstg + " ") * 3
+print(fstg, type(fstg))
+print(manywd, type(manywd))
+
+# third example
+
+nd_a = 6
+nb_b = nd_a + 87
+nb_c = nb_b % 2
+print(nb_b, type(nb_b))
+print(nb_c, type(nb_c))
+
+# fourth example
+
+greetings = "Hello!" + " " + "How are you today?"
+print(greetings, type(greetings))
+
+# I hope everything is correct
