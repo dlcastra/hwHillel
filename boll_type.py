44e21adf7af4151dccc9f4b13b2d9d696446ca2e
dlcastra <1dlcastra@gmail.com>
2022-07-31 21:55:14 +0300

    I completed the first task

---
+++ boll_type.py
@@ -0,0 +1,47 @@
+# first task
+
+comparison_nuber: bool = 3 < 5
+comparison_nuber1: bool = 3 != 5
+
+print(comparison_nuber)
+print(comparison_nuber1)
+
+# second task
+
+comparison_nuber2: bool = 5 == 5
+comparison_nuber3: bool = 5 >= 5
+comparison_nuber4: bool = 5 <= 5
+
+print(comparison_nuber2)
+print(comparison_nuber3)
+print(comparison_nuber4)
+
+# third task
+
+lg_oper = True or False
+lg_oper1 = not False and True
+lg_oper2 = False or (True and True)
+lg_oper3 = not False and not True or True
+lg_oper4 = False or not not True
+
+print(lg_oper)
+print(lg_oper1)
+print(lg_oper2)
+print(lg_oper3)
+print(lg_oper4)
+
+# four task
+
+comp_nb_with_oper = None != 7
+
+print(comp_nb_with_oper)
+
+# fifth task
+
+comp_str_with_exp = bool("") < 10 - 1
+comp_str_with_exp1 = bool("") <= 10 - 1
+comp_str_with_exp2 = bool("") != 10 - 1
+
+print(comp_str_with_exp)
+print(comp_str_with_exp2)
+
---
+++ rows_and_numbers_advance.py
@@ -0,0 +1,2 @@
+one_space_str = bool("") or bool("")  # '', '''''', """""" ||| True or False
+print(f"{one_space_str=}")
