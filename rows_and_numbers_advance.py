20f5329c7a28e9815406a24cb7df569659a60f06
dlcastra <1dlcastra@gmail.com>
2022-07-31 22:49:45 +0300

    I completed the second task

--- rows_and_numbers_advance.py
+++ rows_and_numbers_advance.py
@@ -1,2 +1,10 @@
-one_space_str = bool("") or bool("")  # '', '''''', """""" ||| True or False
-print(f"{one_space_str=}")
+user_name = str(input("Hi, tell mi your name:"))
+removing_spaces = user_name.strip()
+correct_reg = removing_spaces.capitalize()
+greetings = f"Hi {correct_reg}, you are so cool"
+nb_in_name = len(correct_reg)
+
+print(greetings)
+print("See how i can")
+print("Your name consists of",nb_in_name,"letters")
+print("I swap your name:", correct_reg[::-1])
