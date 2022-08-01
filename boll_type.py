2030e1a591e6d77ec97686f2b741c439f942d5e0
dlcastra <1dlcastra@gmail.com>
2022-08-01 15:52:03 +0300

    correct

--- boll_type.py
+++ boll_type.py
@@ -1,28 +1,40 @@
-# first task
+#first task
 
 comparison_nuber: bool = 3 < 5
-comparison_nuber1: bool = 3 != 5
 
 print(comparison_nuber)
+
+#second task
+
+comparison_nuber1: bool = 5 == 5
+comparison_nuber2: bool = 5 >= 5
+comparison_nuber3: bool = 5 <= 5
+comparison_nuber4: bool = not 5 < 5
+comparison_nuber5: bool = not 5 > 5
+comparison_nuber6: bool = not 5 != 5
+comparison_nuber7: bool = not 5 == (not 5)
+comparison_nuber8 = bool(5) or bool(5)
+comparison_nuber9 = bool(5) or not bool(5)
+comparison_nuber10 = bool(5) and bool(5)
+
 print(comparison_nuber1)
-
-# second task
-
-comparison_nuber2: bool = 5 == 5
-comparison_nuber3: bool = 5 >= 5
-comparison_nuber4: bool = 5 <= 5
-
 print(comparison_nuber2)
 print(comparison_nuber3)
 print(comparison_nuber4)
+print(comparison_nuber5)
+print(comparison_nuber6)
+print(comparison_nuber7)
+print(comparison_nuber8)
+print(comparison_nuber9)
+print(comparison_nuber10)
 
-# third task
+# #third task
 
-lg_oper = True or False
-lg_oper1 = not False and True
-lg_oper2 = False or (True and True)
-lg_oper3 = not False and not True or True
-lg_oper4 = False or not not True
+lg_oper = True or True and False
+lg_oper1 = not True and not True or not False
+lg_oper2 = not False or (not True and True)
+lg_oper3 = True and True or not False
+lg_oper4 = True or not True and not False
 
 print(lg_oper)
 print(lg_oper1)
