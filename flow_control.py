039d841906ca5a9241bb3d62be76742d8e1c23c9
dlcastra <1dlcastra@gmail.com>
2022-08-07 22:33:36 +0300

    calculator

--- flow_control.py
+++ flow_control.py
@@ -1,28 +1,53 @@
+# CALCULATOR
 print("instruction:\nSUM is: +\nDIFFERENCE is: -\nMULTIPLICATION is: *\nDIVISION is: /\nEXPONENTIATION is: **")
-
+f_num = input("First number =")
+s_num = input("Second number =")
 operation = input("Your operation is: ")
-f_num = float(input("First number ="))
-s_num = float(input("Second number ="))
 
-if operation == '+':
-    result = f_num + s_num
-    print("Result =", result)
+try:
+    f_num = int(f_num)
+    s_num = int(s_num)
+    if operation == '+':
+        result = f_num + s_num
+        # print(result)
+    elif operation == '-':
+        result = f_num - s_num
+        # print(result)
+    elif operation == '*':
+        result = f_num * s_num
+        # print(result)
+    elif operation == '/':
+        result = f_num / s_num
+        # print(result)
+    elif operation == '**':
+        result = f_num ** s_num
+        # print(result)
+    else:
+        print("Error: An invalid character was entered!")
+    print(f'You result = {result}', type(result))
 
-elif operation == '-':
-    result = f_num - s_num
-    print("Result =", result)
+except ZeroDivisionError:
+    print('Are you serious?1')
 
-elif operation == '*':
-    result = f_num * s_num
-    print("Result =", result)
+except ValueError:
+    f_num = float(f_num)
+    s_num = float(s_num)
+    if operation == '+':
+        result = f_num + s_num
+        # print(result)
+    elif operation == '-':
+        result = f_num - s_num
+        # print(result)
+    elif operation == '*':
+        result = f_num * s_num
+        # print(result)
+    elif operation == '/':
+        result = f_num / s_num
+        # print(result)
+    elif operation == '**':
+        result = f_num ** s_num
+        # print(result)
+    else:
+        print("Error: An invalid character was entered!")
+    print(f'You result = {result}', type(result))
 
-elif operation == '/':
-    result = f_num / s_num
-    print("Result =", result)
-
-elif operation == '**':
-    result = f_num ** s_num
-    print("Result =", result)
-
-else:
-    print(f"Error: An invalid character was entered!")
