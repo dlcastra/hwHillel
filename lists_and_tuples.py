eb3d44466e6913ba142ef4c2ac6b3fc6911d1365
dlcastra <1dlcastra@gmail.com>
2022-08-17 21:18:16 +0300

    90% of the task completed

--- lists_and_tuples.py
+++ lists_and_tuples.py
@@ -1,5 +1,24 @@
-# value = input(":=").split()
-# print(value[2::3])
-#
-# some_list = [1, 2.1, "f", "2", 3, "1", 18, "df"]
+# First task
 
+value = input("Input your sentence").split()
+
+print(value[2::3])
+
+# Second task
+
+input_list = [1, 2.1, "f", "2", 3, "1", 18, "df"]
+
+complete_list = [element if type(element) == float
+
+            else element if (type(element) == int and element % 2 == 0)
+
+            else element ** 2 if (type(element) == int and element % 2 != 0)
+
+            else int(element) * 3 if type(element) == str and element.isdigit()
+
+            else -1
+
+            for element in input_list
+]
+
+print(f"Change list: {complete_list}")  # [1, 2.1, -1, '6', 9, '3', 18, -1]
