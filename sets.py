e8213c76646bf005d1fff8cea2680f47f6434f45
dlcastra <1dlcastra@gmail.com>
2022-08-26 14:59:57 +0300

    I'm something compleat

--- sets.py
+++ sets.py
@@ -1,5 +1,5 @@
 # First task
-
+#
 first_set = input("Input something")
 second_set = input("Input something")
 
@@ -14,7 +14,19 @@ print(list(operation_with_letters))
 
 # Second task
 
-# first_set = {'1', '2', '3', '4', '5'}
-# second_set = {'2', '4', '6', '8', '10', '12'}
+first_set = {'1', '2', '3', '4', '5',16}
+second_set = {'2', '4', '6', '8', '10', '12',16}
+third_set = {0,11,13,15,16}
+# first_set = {1, 2, 3, 4, 5, 16}
+# second_set = {2, 4, 6, 8, 10, 12, 16}
+# third_set = {0, 11, 13, 15, 16,}
 
+first_set |= second_set | third_set
+second_set &= first_set & third_set
+third_set -= second_set - third_set
+op = first_set ^ second_set ^ third_set
 
+print(first_set)
+print(second_set)
+print(third_set)
+print(op)
