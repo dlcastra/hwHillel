501477e991c9fce1339e4cca7c14ef811569861c
dlcastra <1dlcastra@gmail.com>
2022-09-11 19:46:07 +0300

    100% is complete

---
+++ requests/requests_practice.py
@@ -0,0 +1,50 @@
+import requests
+import json
+
+# Task 4.1
+
+url = "https://jsonplaceholder.typicode.com/posts/1"
+
+reading = requests.get(url)
+save_data = reading.json()
+
+with open('request.json', 'w') as save_in_json:
+    json.dump(save_data, save_in_json, indent=4)
+
+print(f"{reading.status_code = }")
+print(f"{save_data = }\n")
+
+# Task 4.2
+
+with open('request.json', 'r') as read_and_save_json:
+    response_data = json.load(read_and_save_json)
+
+    put = requests.put(url, data=response_data)
+
+print(f'{response_data = }')
+print(f'{put.status_code = }\n')
+
+# Task 4.3
+
+some_url = 'https://stackoverflow.com/'
+########################################################################################################################
+
+query = requests.get(some_url)
+change_type = query.text
+
+with open('query.url', 'w', encoding='utf-8') as url_code:
+    url_code.write(change_type)
+
+########################################################################################################################
+
+query_1 = requests.get(some_url)
+encoding_type = query_1.encoding
+print(encoding_type)
+
+########################################################################################################################
+#Task 5
+
+picture = requests.get("https://moya-planeta.ru/upload/images/xl/c2/11/c2118c599348f3ce7bbcfe5e9696a0bb.jpg")
+
+with open('photo.jpg', 'wb') as sunset:
+    sunset.write(picture.content)
