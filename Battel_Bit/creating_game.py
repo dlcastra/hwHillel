e602841d3f2c645974584895c6dccc8bf1d44d61
dlcastra <1dlcastra@gmail.com>
2022-09-13 09:56:17 +0300

    100% is complete

---
+++ Battel_Bit/creating_game.py
@@ -0,0 +1,24 @@
+import random
+
+
+class BattelUnit():
+
+    heals = 100
+    armor = 100
+    damage = random.randint(5,50)
+
+class Archer(BattelUnit):
+
+    pass
+
+class Witch(BattelUnit):
+
+    pass
+
+class Knight(BattelUnit):
+
+    pass
+
+
+
+
--- requests/requests_practice.py
+++ requests/requests_practice.py
@@ -5,46 +5,72 @@ import json
 
 url = "https://jsonplaceholder.typicode.com/posts/1"
 
-reading = requests.get(url)
-save_data = reading.json()
 
-with open('request.json', 'w') as save_in_json:
-    json.dump(save_data, save_in_json, indent=4)
+def saving_in_json():
+    reading = requests.get(url)
+    save_data = reading.json()
 
-print(f"{reading.status_code = }")
-print(f"{save_data = }\n")
+    with open('request.json', 'w') as save_in_json:
+        json.dump(save_data, save_in_json, indent=4)
+
+    print(f"{reading.status_code = }")
+    print(f"{save_data = }\n")
+
+
+saving_in_json()
+
 
 # Task 4.2
 
-with open('request.json', 'r') as read_and_save_json:
-    response_data = json.load(read_and_save_json)
+def read_and_save():
+    with open('request.json', 'r') as read_and_save_json:
+        response_data = json.load(read_and_save_json)
 
-    put = requests.put(url, data=response_data)
+        put = requests.put(url, data=response_data)
 
-print(f'{response_data = }')
-print(f'{put.status_code = }\n')
+    print(f'{response_data = }')
+    print(f'{put.status_code = }\n')
 
+
+read_and_save()
 # Task 4.3
 
 some_url = 'https://stackoverflow.com/'
-########################################################################################################################
 
-query = requests.get(some_url)
-change_type = query.text
-
-with open('query.url', 'w', encoding='utf-8') as url_code:
-    url_code.write(change_type)
 
 ########################################################################################################################
 
-query_1 = requests.get(some_url)
-encoding_type = query_1.encoding
-print(encoding_type)
+def html_code():
+    query = requests.get(some_url)
+    change_type = query.text
+
+    with open('query.url', 'w', encoding='utf-8') as url_code:
+        url_code.write(change_type)
+
+
+html_code()
+
 
 ########################################################################################################################
-#Task 5
 
-picture = requests.get("https://moya-planeta.ru/upload/images/xl/c2/11/c2118c599348f3ce7bbcfe5e9696a0bb.jpg")
+def encod_type():
+    query_1 = requests.get(some_url)
+    encoding_type = query_1.encoding
+    print(encoding_type)
 
-with open('photo.jpg', 'wb') as sunset:
-    sunset.write(picture.content)
+
+encod_type()
+
+
+########################################################################################################################
+
+# Task 5
+
+def download_pic():
+    picture = requests.get("https://moya-planeta.ru/upload/images/xl/c2/11/c2118c599348f3ce7bbcfe5e9696a0bb.jpg")
+
+    with open('photo.jpg', 'wb') as sunset:
+        sunset.write(picture.content)
+
+
+download_pic()
